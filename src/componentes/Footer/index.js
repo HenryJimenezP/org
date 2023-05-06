1a6b4791df1c25d6e115c22ec319544b54361639
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2023-05-06 16:47:58 -0700

    Actualizado Footer

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -3,18 +3,18 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='#'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://twitter.com/ing_henryjp95'>
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/ing.henryjp29/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Henry</strong>
     </footer>
 }
 
